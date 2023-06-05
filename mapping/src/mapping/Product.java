143434c61b7cd7023e5d1f480d9a68162eef03cc
Debjyoti-Chandra-Das <134958985+Debjyoti-Chandra-Das@users.noreply.github.com>
2023-06-05 15:49:33 +0530

    Add files via upload

---
+++ mapping/src/mapping/Product.java
@@ -0,0 +1,14 @@
+package mapping;
+
+public class Product {
+    String productId;
+    String name;
+    Product(String productId, String name){
+    	this.productId = productId;
+    	this.name=name;
+    }
+	@Override
+	public String toString() {
+		return "[productId=" + productId + ", name=" + name + "]";
+	}
+}
